fddf8e9c2c55cf25756a35a5a1d83cabd3820d18
msacristan <miguel.sacristan@gmail.com>
2015-11-02 00:40:35 +0100

    server changes

--- tasks/server.js
+++ tasks/server.js
@@ -1,18 +1,35 @@
 module.exports = function(grunt){
     var express = require("express");
   grunt.registerTask("server", "static file development server", function(){
-    var app, webPort, webRoot;
-      webPort = grunt.config.get("server.web.port") || 8000;
-      webRoot = grunt.config.get("server.base") || "dist";
+     var app, webPort, webRoot;
+     webPort = grunt.config.get("server.web.port") || 8000;
+     webRoot = grunt.config.get("server.base") || "dist";
 
-      app = express();
-      //app.use(express.compress()); // no longer in express
-      //app.use(express.errorhandler()); // no longer in express
-      app.use(express.static("" + (process.cwd()) + "/" + webRoot));
-      app.listen(webPort);
-      grunt.log.writeln("Starting server here...");
+     app = express();
+     //app.use(express.compress()); // no longer in express
+     //app.use(express.errorhandler()); // no longer in express
+     //app.use(express.urlencoded());
+     // middleware
 
-      return app;
+
+     app.get("/secret", function(request, response){
+       response.send({
+         foo: "bar"
+       });
+     });
+
+     app.post("/doStuff", function(request, response){
+       var param = request.param("foo");
+       response.send({
+         foo: param
+       });
+     });
+
+     app.use(express.static("" + (process.cwd()) + "/" + webRoot));
+     app.listen(webPort);
+     grunt.log.writeln("Starting server here...");
+
+     return app;
 
   });
 };
